27184c7cd50aa155dce71d679dcada254bb42bbd
abdelkaderlagha <abdelkaderlaghaa@outlook.fr>
2020-11-13 00:22:04 +0100

    Initial start

---
+++ src/main/java/gadour/springframework/spring5jokesapp/Spring5JokesAppApplication.java
@@ -0,0 +1,13 @@
+package gadour.springframework.spring5jokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5JokesAppApplication.class, args);
+    }
+
+}
