92094a03ddf988e1c74521fdac81f72e35480434
abdelkaderlagha <abdelkaderlaghaa@outlook.fr>
2020-11-13 00:57:12 +0100

    added the controller. closes #3

---
+++ src/main/java/gadour/springframework/spring5jokesapp/controller/JokeController.java
@@ -0,0 +1,25 @@
+package gadour.springframework.spring5jokesapp.controller;
+
+import gadour.springframework.spring5jokesapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke" , jokeService.getJokes());
+        return "chucknorris";
+    }
+}
